0ade7977564875baa48f85a41643abd66278f023
ioachim-hub <61659267+ioachim-hub@users.noreply.github.com>
2021-05-09 18:37:25 +0300

    models

--- hello/models.py
+++ hello/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Sentiment(models.Model):
-  link     = model.TextField(max_length=200, null=True)
-  data     = model.DateField()
-  rezultat = model.TextField(max_length=300, null=True)
+  link     = models.TextField(max_length=200, null=True)
+  data     = models.DateField()
+  rezultat = models.TextField(max_length=300, null=True)
