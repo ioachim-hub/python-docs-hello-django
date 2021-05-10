077c1a194f1bcf5c320b6273dc473e01591d6c8d
ioachim-hub <61659267+ioachim-hub@users.noreply.github.com>
2021-05-10 09:07:36 +0300

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -83,7 +83,7 @@ def hello_submit(request):
         blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
         
         with open(local_file_name, "rb") as data:
-            blob_client.upload_blob(bytes(data, 'utf-8'))
+            blob_client.upload_blob(data)
             
         link = blob_client.url
         
