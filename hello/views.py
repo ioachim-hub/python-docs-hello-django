f0c455e0927f44abd8bcf28a7bab8056f7207e27
ioachim-hub <61659267+ioachim-hub@users.noreply.github.com>
2021-05-10 11:48:13 +0300

    add reverse

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from .models import Sentiment
 
 import datetime
