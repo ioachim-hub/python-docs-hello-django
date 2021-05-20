76cae5d1a90d31a7ff19e0302e90f0c7c38fbb75
ioachim-hub <61659267+ioachim-hub@users.noreply.github.com>
2021-05-20 18:17:35 +0300

    Update settings.py

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'django_hello.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {    
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }    
-# }
+DATABASES = {    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }    
+}
+
+"""
 DATABASES = {
 
     'default': {
@@ -91,6 +93,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
